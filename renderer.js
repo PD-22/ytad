1a0a1d870825752950bbb065c60615e3c8fd9704
Pavle <p.devidze@gmail.com>
2024-08-10 03:27:49 +0400

    add style

--- renderer.js
+++ renderer.js
@@ -3,27 +3,24 @@ const input = document.getElementsByTagName('input')[0];
 const list = document.getElementsByTagName('ul')[0];
 
 form.addEventListener('submit', async event => {
-    let link, title;
+    let link
     try {
         event.preventDefault();
         link = input.value;
         if (typeof link !== 'string' || !link) throw new Error('Invalid link');
-        log('Checking: ', link)
+        log('Opening: ', link)
 
-        title = await window.api.title(link)
+        const title = await window.api.title(link)
         if (typeof title !== 'string' || !title) throw new Error('Invalid title');
 
-        log(`Locating: "${title}"`);
         const output = await window.api.location(title);
         if (typeof output !== 'string' || !output) throw new Error('Invalid output');
 
-        log(`Downloading: ${link} - "${title}" to ${output}`);
+        log(`Downloading: `, link);
         const result = await window.api.start(link, output);
         log(`Result: `, result);
     } catch (error) {
-        if (title)
-            log(`Error: ${title}`);
-        else if (link)
+        if (link)
             log('Error: ', link);
         else
             log('Error');
