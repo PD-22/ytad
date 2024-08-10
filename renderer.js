d8d22c5aa4644211a52214eb9ea1892d7a7be600
Pavle <p.devidze@gmail.com>
2024-08-10 17:33:53 +0400

    add external button

--- renderer.js
+++ renderer.js
@@ -1,6 +1,16 @@
 const form = document.getElementsByTagName('form')[0];
 const input = document.getElementsByTagName('input')[0];
+const button = document.getElementsByTagName('button')[0];
 const ul = document.getElementsByTagName('ul')[0];
+
+const externalLink = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link">
+    <path d="M15 3h6v6" />
+    <path d="M10 14 21 3" />
+    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+</svg>
+`;
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
@@ -23,9 +33,9 @@ form.addEventListener('submit', async event => {
 
         const folder = await window.api.start(link, output);
         if (typeof folder !== 'string' || !folder) throw new Error('Invalid Folder');
-        li.textContent = `Link: ${link}\nTitle: ${title}\nOutput: `;
+        li.textContent = `Link: ${link}\nTitle: ${title}\nOutput: ${output}`;
         const a = document.createElement('a');
-        a.textContent = output;
+        a.innerHTML = externalLink;
         a.href = folder;
         li.append(a);
     } catch (error) {
